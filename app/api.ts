1e8e83fdbba66cb8e7cf95aed5c4f52002cd07c7
alexhr05 <56800932+alexhr05@users.noreply.github.com>
2025-11-09 23:59:34 +0200

    Made fetchDevices GET function

--- app/api.ts
+++ app/api.ts
@@ -1,6 +1,11 @@
 export async function fetchDevices(){
     try{
-
+        const response = await fetch("https://www.bgroutingmap.com/8/houmTaimerApk.php?m=14");
+        if(!response.ok){
+            throw new Error("HTTP error: " + response.status);
+        }
+        const data = await response.json();
+        return data;
     }catch(err){
         console.log("Грешка при заявката:",err);
         throw err;
